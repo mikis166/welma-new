fed641aefe00bc7b47c7994ce5a922400e1fd4b1
Petros Kyriakou <9pitops@gmail.com>
2017-04-30 16:23:12 +0300

    added markdown to job description - halfway through show action for jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,8 @@
 class Job < ActiveRecord::Base
+  before_save :save_html
+
+  def save_html
+  	md  = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new, extensions = {})
+  	write_attribute :description, md.render(description)
+  end
 end
